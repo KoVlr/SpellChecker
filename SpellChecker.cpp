814dcd6af8ea9513af6ca38aa45add617223278d
KoVlr <wladimir.konowalow411@mail.ru>
2022-07-24 19:31:13 +0300

    hash function added

--- SpellChecker.cpp
+++ SpellChecker.cpp
@@ -16,33 +16,58 @@ struct Substitutions
     int N;
 };
 
+unsigned int hash(char* spell)
+{
+    unsigned int res = 0;
+    unsigned int coefficient = 1;
+    int i = 0;
+    char c = spell[i];
+    while(c != '\0')
+    {
+        if(c != '>')
+        {
+            res += (unsigned)c * coefficient;
+            coefficient *= 256;
+        }
+        else coefficient = 65536;
+        i++;
+        c = spell[i];
+    }
+    return res;
+}
+
 char* SpellCheckCpp(char* typo, Voctrie* voctrie, Substitutions* substitutions)
 {
-    std::unordered_map<std::string, double> submap;
+    std::unordered_map<int, double> submap;
     for(int i = 0; i < substitutions->N; i++)
     {
-            submap.insert(std::make_pair(std::string(substitutions->spell[i]), substitutions->p[i]));
+            submap.insert(std::make_pair(hash(substitutions->spell[i]), substitutions->p[i]));
     }
 
     double Mp[22][22];
     double maxp = 0;
     char* output = "-ERROR-";
+    int typolen = strlen(typo);
     for(int k = 0; k < voctrie->N; k++)
     {
         char* vocword = voctrie->word[k];
         int i0 = voctrie->prelen[k];
         char spell[22];
-        double p = 0;
-        for(int i = i0; i < int(strlen(vocword)); i++)
+        double p;
+        int voclen = strlen(vocword);
+        for(int i = i0; i < voclen; i++)
         {
-            for(int j = -1; j < int(strlen(typo)); j++)
+            for(int j = -1; j < typolen; j++)
             {
-                strncpy(spell, vocword, i+1);
-                spell[i+1] = '>';
-                strncpy(spell + i+2, typo, j+1);
-                spell[i+j+3] = '\0';
-                if(submap.find(std::string(spell)) != submap.end()) p = submap.at(std::string(spell));
-                else p = 0;
+                p = 0;
+                if((i<2) && (j<2))
+                {
+                    strncpy(spell, vocword, i+1);
+                    spell[i+1] = '>';
+                    strncpy(spell + i+2, typo, j+1);
+                    spell[i+j+3] = '\0';
+                    if(submap.find(hash(spell)) != submap.end()) p = submap.at(hash(spell));
+                }
                 for(int n = std::max(i-2, -1); n < i+1; n++)
                 {
                     for(int m = std::max(j-2, -1); m < j+1; m++)
@@ -54,18 +79,11 @@ char* SpellCheckCpp(char* typo, Voctrie* voctrie, Substitutions* substitutions)
                             strncpy(spell + i-n+1, typo + m + 1, j-m);
                             spell[i-n+j-m+1] = '\0';
                             double nextP = 0;
-                            if(submap.find(std::string(spell)) != submap.end())
+                            if(submap.find(hash(spell)) != submap.end())
                             {
-                                nextP = Mp[n+1][m+1] * submap.at(std::string(spell));
+                                nextP = Mp[n+1][m+1] * submap.at(hash(spell));
                             }
-                            else
-                            {
-                                nextP = 0;
-                            }
-                            if(nextP > p)
-                            {
-                                p = nextP;
-                            }
+                            if(nextP > p) p = nextP;
                         }
                     }
                 }
